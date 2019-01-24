ad4f71126043efc26b0f203d2965ef08d4777fde
scottfraser4700 <drbland.64000@gmail.com>
2019-01-24 15:39:47 -0500

    second lab session, much progress

--- LJ.m
+++
@@ -1,7 +0,0 @@
-function [Phi,dPhidr] = LJ(r, Epsilon, Sigma)
-%LJ returns leonard jones potential for a given atomic configuration
-
-Phi = 4*Epsilon*(Sigma^12*r^-12 - Sigma^6*r^-6);
-dPhidr = 4*Epsilon*(Sigma^12*(-12)*r^-13);
-end
-
