3e922d505e9eef8aad5430b5700851098391e91e
Scott <scottmfraser@cmail.carleton.ca>
2019-01-23 20:11:54 -0500

    jan 23rd homework session changes

---
+++ updateVelocity.m
@@ -0,0 +1,11 @@
+function [atomsVX, atomsVY] = updateVelocity(atomsVX, atomsVY)
+%UpdateVelocity Updates the X and Y velocity components for all involved
+%atoms
+%   using the velocities, and forces as inputs, we will use the equations
+%   of motion to return vectors of the velocities
+%   Interacts with the getforces function, and is called in the update
+%   position function. this function should also implement the boundary
+%   conditions
+
+end
+
