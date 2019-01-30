1942f693b759950b67150f3351c1038c5fccffa8
Scott <scottmfraser@cmail.carleton.ca>
2019-01-29 22:00:56 -0500

    working from home, almost done part 1

--- Transport.m
+++ Transport.m
@@ -10,11 +10,7 @@ frameHeight = 100e-9;
 Vth = sqrt(C.kb * C.T / (0.26*C.m_0));
 Tstop = 1e-10;
 t = 0;
-dt = 1e-12;
-%setting up frame
-figure
-xlim([0 frameWidth])
-ylim([0 frameHeight])
+dt = 1e-14;
 %setting up global variables, shame me later
 global nAtoms;      %number of particles used
 global X;           %x positions of all particles
@@ -23,13 +19,10 @@ global VX;          %X component of velocities
 global VY;          %Y component of velocities
 global Xnext;       %Next x coordinate of particles
 global Ynext;       %Next y coordinate of particles
-global Xp;          %previous x coord
-global Yp;          %previous y coord
-
-
+global EK;
+global Temperature;
+%initialization of vectors
 nAtoms = 100;
-Xp = zeros(1,nAtoms);
-Yp = zeros(1,nAtoms);
 Xnext = zeros(1,nAtoms);
 Ynext = zeros(1,nAtoms);
 direction = 2*pi*rand(1, nAtoms);
@@ -37,36 +30,72 @@ VX = Vth .* cos(direction);
 VY = Vth .* sin(direction);
 X = frameWidth .* rand(1, nAtoms);
 Y = frameHeight .* rand(1, nAtoms);
-History
-
+EK = 0;
+Temperature = 0;
 %electrons placed randomly, with uniform velocity and random directions
 
 while t < Tstop
     %for each cycle in time, move electrons in accordance with their
     %velocities
-    Xp = X;
-    Yp = Y;
+   
     Xnext = X + VX*dt;
     Ynext = Y + VY*dt;
-    %X boundary conditions set, and x positions updated
+    %X boundary conditions set
     right = Xnext>frameWidth;
     left = Xnext<0;
     Xnext(right) = Xnext(right)-frameWidth;
     Xnext(left) = Xnext(left) + frameWidth;
-    X = Xnext;
-    Y = Ynext;
     %Y boundary conditions set
     top = Ynext > frameHeight;
     bottom = Ynext < 0;
     VY(top | bottom) = VY(top | bottom) * -1;
+    %EK = mean(VX.*VX+VY.*VY);
+    %Temperature = EK / nAtoms / C.kb;
+    %plotting, but trying to avoid plotting the full horizontal jump
+    
+   %subplot(2,1,2); 
+   %figure(2)
+  %plot(t, Temperature);
+   %hold on
+   %xlim([0 1e-10]) 
+  % ylim([0 1e32])
     
     
+    if abs((Xnext(1) - X(1)) < 2*abs(VX(1))*dt)
+        %subplot(2,1,1);
+        figure(1)
+        plot([Xnext(1) X(1)], [Ynext(1) Y(1)], 'blue')
+        xlim([0 frameWidth])
+        ylim([0 frameHeight])
+        hold on
+    end
+    if abs((Xnext(2) - X(2)) < 2*abs(VX(2))*dt)
+        %subplot(2,1,1);
+        figure(1)
+        plot([Xnext(2) X(2)], [Ynext(2) Y(2)], 'red')
+        xlim([0 frameWidth])
+        ylim([0 frameHeight])
+        hold on
+    end
+    if abs((Xnext(3) - X(3)) < 2*abs(VX(3))*dt)
+        %subplot(2,1,1);
+        figure(1)
+        plot([Xnext(3) X(3)], [Ynext(3) Y(3)], 'green')
+        xlim([0 frameWidth])
+        ylim([0 frameHeight])
+        hold on
+    end
+    
+    
+    %updating positions, and advancing time a step forward so the while
+    %loop works
+    X = Xnext;
+    Y = Ynext;
     t = t+dt;
-    pause(0.1);
-    plot([Xp(1:3) X(1:3)], [Yp(1:3) Y(1:3)], 'b')
-    %scatter(X(1,:), Y(1,:))
-    xlim([0 frameWidth])
-    ylim([0 frameHeight])
+    
+ 
+    pause(0.01);
+    
+    
 end
 %TODO add calculations for temperature, and plot temperature
-%TODO reimplement plotting setup
